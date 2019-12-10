94525e5457d5a09efa768caffb8b5bb663269b00
Void-sys <JeanoBest2904>
2019-12-10 17:32:27 +0700

    Login Authentication using Firebase.
    NOTES : MAKE SURE TO CONNECT THE FIREBASE FIRST AND MAKE THE CREDENTIALS AT THE FIREBASE CONSOLE IN WEB, i'll send the credentials and DB name at socmed !!!!

--- app/src/main/java/com/example/dokterrkuu/LoginActivity.java
+++ app/src/main/java/com/example/dokterrkuu/LoginActivity.java
@@ -1,14 +1,66 @@
 package com.example.dokterrkuu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText user,pass;
+    Button log;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        user = (EditText) findViewById(R.id.Username);
+        pass = (EditText) findViewById(R.id.Password);
+        log = (Button) findViewById(R.id.LogButt);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+
+                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+                    user.setError("Please Fill The Empty Field(s) !!");
+                    return;
+                }
+
+                //AUTHENTICATION
+
+                firebaseAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Username Not Exist OR Wrong Credentials", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
 }
