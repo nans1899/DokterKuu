e519ea6c201037b439129b1fb8b20d51ae931a42
nans1899 <anandabakti18@gmail.com>
2019-12-10 20:32:30 +0700

    10/12/19

--- app/src/main/java/com/example/dokterrkuu/MainActivity.java
+++ app/src/main/java/com/example/dokterrkuu/MainActivity.java
@@ -5,32 +5,17 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView userdash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    Intent intent = getIntent();
-
-    String getUser = intent.getStringExtra(LoginActivity.EXTRA_TEXT);
-
-    userdash = (TextView)findViewById(R.id.txtdashboard);
-
-    userdash.setText(getUser);
-
-
         ImageView c_mata = findViewById(R.id.mataklik);
 
         c_mata.setOnClickListener(new View.OnClickListener() {
