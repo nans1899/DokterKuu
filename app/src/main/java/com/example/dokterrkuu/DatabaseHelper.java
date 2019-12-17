f4fca602c5e2dc4187cf42f166079659e1400c5e
Void-sys <JeanoBest2904>
2019-12-18 03:03:43 +0700

    ADD DATE FEATURE, ADD DISEASE TYPE, MORE DOCTORS,   AND ADD  DISEASE TYPE TO DB, 18/12/19 3:04PM

--- app/src/main/java/com/example/dokterrkuu/DatabaseHelper.java
+++ app/src/main/java/com/example/dokterrkuu/DatabaseHelper.java
@@ -14,8 +14,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "Users";
     public static final String COL_1 = "Username";
     public static final String COL_2 = "Date";
-    public static final String COL_3 = "docName";
-    public static final String COL_4 = "hospital";
+    public static final String COL_3 = "Disease";
+    public static final String COL_4 = "docName";
+    public static final String COL_5 = "hospital";
+
 
 
     public DatabaseHelper(Context context) {
@@ -26,7 +28,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("create table " + TABLE_NAME + " (Username TEXT PRIMARY KEY, docName TEXT, Hospital TEXT)");
+        db.execSQL("create table " + TABLE_NAME + " (Username TEXT PRIMARY KEY, Date DATE, disease TEXT,docName TEXT, Hospital TEXT)");
 
     }
 
@@ -36,12 +38,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData(String name, String docname, String hospital){
+    public boolean insertData(String name, Date Date,String disease,String docname, String hospital){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_1, name);
-        contentValues.put(COL_3, docname);
-        contentValues.put(COL_4, hospital);
+        contentValues.put(COL_2, Date.toString());
+        contentValues.put(COL_3, disease);
+        contentValues.put(COL_4, docname);
+        contentValues.put(COL_5, hospital);
         long result = db.insert(TABLE_NAME, null, contentValues);
 
         if(result == -1){
@@ -49,7 +53,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }else{
             return true;
         }
-
     }
 
 }
