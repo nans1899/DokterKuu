3ba8f58a38471fa8e33186881c7b34b1fa3af204
nans1899 <anandabakti18@gmail.com>
2019-12-17 21:02:21 +0700

    10/12/19

--- app/src/main/java/com/example/dokterrkuu/PengeheliatanActivity.java
+++ app/src/main/java/com/example/dokterrkuu/PengeheliatanActivity.java
@@ -2,7 +2,10 @@ package com.example.dokterrkuu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PengeheliatanActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class PengeheliatanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengeheliatan);
+
+        Button but1 = findViewById(R.id.janjibutt);
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pengheliatan = new Intent(PengeheliatanActivity.this, DocAppointment.class);
+                startActivity(pengheliatan);
+            }
+        });
     }
 }
