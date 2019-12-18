ff307422137753964ece5359743751e36b039b8c
nans1899 <anandabakti18@gmail.com>
2019-12-18 20:02:57 +0700

    10/12/19

--- app/src/main/java/com/example/dokterrkuu/DocAppointment.java
+++ app/src/main/java/com/example/dokterrkuu/DocAppointment.java
@@ -2,6 +2,7 @@ package com.example.dokterrkuu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -28,6 +29,16 @@ public class DocAppointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_appointment);
+
+        Button btn1 = findViewById(R.id.CekButton);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DocAppointment.this, ShowAppointment.class);
+                startActivity(intent);
+            }
+        });
 
         //CALLING DATABASEHELPER
         databaseHelper = new DatabaseHelper(this);
