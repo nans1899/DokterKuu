0e14c1c2ddfb9f458c73a7937c11c10055d8d88c
nans1899 <anandabakti18@gmail.com>
2019-12-19 04:14:01 +0700

    10/12/19

--- app/src/main/java/com/example/dokterrkuu/MainActivity.java
+++ app/src/main/java/com/example/dokterrkuu/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +18,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView c_mata = findViewById(R.id.mataklik);
+        ImageView c_gigi = findViewById(R.id.gigiklik);
+        ImageView c_pernafasan = findViewById(R.id.pernafasanklik);
+        ImageView c_pencernaan = findViewById(R.id.pencernaanKlik);
+        ImageView c_kepala = findViewById(R.id.kepalaklik);
+        ImageView c_jantung = findViewById(R.id.jantungklik);
+        ImageView c_kuping = findViewById(R.id.kupingklik);
+
+
+
 
         c_mata.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent MataKlik = new Intent (MainActivity.this, SecondActivity.class);
+                Intent MataKlik = new Intent (MainActivity.this, PengeheliatanActivity.class);
                 startActivity(MataKlik);
+            }
+        });
+
+
+
+        c_gigi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent GigiKlik = new Intent (MainActivity.this, GigiActivity.class);
+                startActivity(GigiKlik);
+            }
+        });
+
+
+        c_pernafasan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent PernafasanKlik = new Intent (MainActivity.this, PernafasanActivity.class);
+                startActivity(PernafasanKlik);
+            }
+        });
+
+
+
+        c_pencernaan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent PencernaannKlik = new Intent (MainActivity.this, PencernaanAcivity.class);
+                startActivity(PencernaannKlik);
+            }
+        });
+
+
+        c_kepala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent KepalaKlik = new Intent (MainActivity.this, KepalaActivity.class);
+                startActivity(KepalaKlik);
+            }
+        });
+
+
+
+        c_jantung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent JantungKlik = new Intent (MainActivity.this, JantungActivity.class);
+                startActivity(JantungKlik);
+            }
+        });
+
+        c_kuping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent KupingKlik = new Intent (MainActivity.this, PendengaranActivity.class);
+                startActivity(KupingKlik);
             }
         });
 
--- app/src/main/java/com/example/dokterrkuu/TulangActivity.java
+++ app/src/main/java/com/example/dokterrkuu/TulangActivity.java
@@ -2,10 +2,7 @@ package com.example.dokterrkuu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class TulangActivity extends AppCompatActivity {
 
@@ -13,14 +10,5 @@ public class TulangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tulang);
-        Button btn1 = findViewById(R.id.janjitulang);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TulangActivity.this, DocAppointment.class);
-                startActivity(intent);
-            }
-        });
     }
 }
