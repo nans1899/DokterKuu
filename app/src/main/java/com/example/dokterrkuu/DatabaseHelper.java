dc2e213296168d88a4fbddda1e3ec1b606e719ac
nans1899 <anandabakti18@gmail.com>
2020-01-09 11:57:53 +0700

    10/12/19

--- app/src/main/java/com/example/dokterrkuu/DatabaseHelper.java
+++ app/src/main/java/com/example/dokterrkuu/DatabaseHelper.java
@@ -84,9 +84,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.close();
         return true;
     }
-    public Integer Delete(String uName){
+    public Integer deleteData(String uName){
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, "Username = ?", new String[] {" uName "});
+        return db.delete(TABLE_NAME, "Username = ?", new String[] {uName});
     }
 
 }
--- app/src/main/java/com/example/dokterrkuu/single_row1.java
+++ app/src/main/java/com/example/dokterrkuu/single_row1.java
@@ -19,26 +19,6 @@ public class single_row1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_row1);
-
-        databaseHelper = new DatabaseHelper(this);
-        delete = (Button) findViewById(R.id.buttonDelete);
-        update = (Button) findViewById(R.id.buttonUpdate);
     }
 
-
-    public void relocate(){
-                Intent intent = new Intent(single_row1.this, DocAppointment.class);
-                startActivity(intent);
-    }
-
-
-    public void delete(){
-                nama = (TextView) findViewById(R.id.viewName);
-                Integer deleteRows = databaseHelper.Delete(nama.getText().toString());
-                if(deleteRows>0){
-                    Toast.makeText(single_row1.this, "Data Successfuly Deleted", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(single_row1.this, "Failed to Delete The Data", Toast.LENGTH_SHORT).show();
-                }
-    }
 }
