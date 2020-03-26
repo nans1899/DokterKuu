96a74fb1bf6dcdf8281972d81d5a960b1bd7837c
= <=>
2020-03-27 03:31:26 +0700

    Fixed Login 27/03/2020 3:32

--- app/src/main/java/com/example/dokterrkuu/LoginActivity.java
+++ app/src/main/java/com/example/dokterrkuu/LoginActivity.java
@@ -36,60 +36,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        Name = (EditText)findViewById(R.id.Username);
-//        Password = (EditText)findViewById(R.id.Password);
-//        Login = (Button)findViewById(R.id.LogButt);
-//
-//        Login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Validate(Name.getText().toString(), Password.getText().toString());
-//            }
-//        });
-//
-//
-//    }
-//
-//    private void Validate(String userName, String userPassword)
-//    {
-//        if( (userName.equals("Ananda Bakti")) && (userPassword.equals("1234")))
-//        {
-//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//            startActivity(intent);
-//        }else
-//        {
-//            Toast.makeText(this, "Username or Password is wrong", Toast.LENGTH_SHORT).show();
-//        }
-//    }
+        Name = (EditText)findViewById(R.id.Username);
+        Password = (EditText)findViewById(R.id.Password);
+        Login = (Button)findViewById(R.id.LogButt);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+}
+
+    private void Validate(String userName, String userPassword)
+    {
+        if( (userName.equals("Ananda Bakti")) && (userPassword.equals("1234")))
+        {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else
+        {
+            Toast.makeText(this, "Username or Password is wrong", Toast.LENGTH_SHORT).show();
+        }
+   }
 
 
-//            public void onClick(View v) {
-//               final String username = user.getText().toString();
-//                String password = pass.getText().toString();
-//
-//                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
-//                    user.setError("Please Fill The Empty Field(s) !!");
-//                    return;
-//                }
-//                    //AUTHENTICATION FIREBASE AUTO
-//
-//                     firebaseAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<AuthResult> task) {
-//                            if(task.isSuccessful()){
-//                                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-//                                Intent intent = new Intent (LoginActivity.this, MainActivity.class);
-//                                startActivity(intent);
-//                            }else{
-//                                Toast.makeText(LoginActivity.this, "Username Not Exist OR Wrong Credentials", Toast.LENGTH_SHORT).show();
-//                            }
-//                        }
-//                    });
-//
-//
-//
-//            }
-//        });
-
-    }
 }
