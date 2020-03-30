8e1d051228d0b56783ffd013bad01ae3dc7fb09e
= <=>
2020-03-30 17:24:33 +0700

    Fixed Intent 17:22

--- app/src/main/java/com/example/dokterrkuu/RecyclerViewPackage/DatabaseRecyclerAdapter.java
+++ app/src/main/java/com/example/dokterrkuu/RecyclerViewPackage/DatabaseRecyclerAdapter.java
@@ -3,12 +3,16 @@ package com.example.dokterrkuu.RecyclerViewPackage;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dokterrkuu.R;
+import com.example.dokterrkuu.single_row1;
 
 import java.util.ArrayList;
 
@@ -20,10 +24,11 @@ public class DatabaseRecyclerAdapter extends RecyclerView.Adapter<DatabaseRecycl
         this.objModelClassArrayList = objModelClassArrayList;
     }
 
+
     @NonNull
     @Override
     public DatabaseViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View singleRow = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.single_row, viewGroup, false);
+        View singleRow = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_single_row1, viewGroup, false);
         return new DatabaseViewHolder(singleRow);
     }
 
@@ -31,11 +36,17 @@ public class DatabaseRecyclerAdapter extends RecyclerView.Adapter<DatabaseRecycl
     public void onBindViewHolder(@NonNull DatabaseViewHolder holder, int position) {
         ModelClass objModelClass = objModelClassArrayList.get(position);
         holder.Name.setText(objModelClass.getName());
-        holder.Date.setText(objModelClass.getName());
-        holder.Disease.setText(objModelClass.getName());
-        holder.docname.setText(objModelClass.getName());
-        holder.hospital.setText(objModelClass.getName());
-        holder.notes.setText(objModelClass.getName());
+        holder.Date.setText(objModelClass.getDate());
+        holder.Disease.setText(objModelClass.getDisease());
+        holder.docname.setText(objModelClass.getDocName());
+        holder.hospital.setText(objModelClass.getHospital());
+        String defaulttext = objModelClass.getKeluhan();
+        if(defaulttext.equals("")){
+            holder.notes.setText("No Basic Complaints or Detailed Information");
+        }else{
+            holder.notes.setText(objModelClass.getKeluhan());
+        }
+
     }
 
     @Override
@@ -45,17 +56,20 @@ public class DatabaseRecyclerAdapter extends RecyclerView.Adapter<DatabaseRecycl
 
     public static class DatabaseViewHolder extends RecyclerView.ViewHolder{
         TextView Name, Date, Disease, docname, hospital, notes;
+
+        Button update, delete;
+
         public DatabaseViewHolder(@NonNull View itemView) {
             super(itemView);
             Name = itemView.findViewById(R.id.viewName);
             Date = itemView.findViewById(R.id.viewDate);
             Disease = itemView.findViewById(R.id.viewDisease);
-            docname = itemView.findViewById(R.id.viewDocName);
-            hospital = itemView.findViewById(R.id.viewHospital);
+            docname = itemView.findViewById(R.id.textviewDocName);
+            hospital = itemView.findViewById(R.id.textviewHospital);
             notes = itemView.findViewById(R.id.viewNotes);
-
-
         }
     }
+
+
 
 }
